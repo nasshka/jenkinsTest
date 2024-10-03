f1945c1494b8031e8108bd17c25542d8d05edc00
Florin Cofei <florin.cofei@gmail.com>
2024-10-03 19:43:14 +0300

    asdsad

---
+++ nashka.com/src/main/java/org/com/nashka/com/App.java
@@ -0,0 +1,13 @@
+package org.com.nashka.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!   asd" );
+    }
+}
